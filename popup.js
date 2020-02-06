849a762e94aaede2dfaff61552bfe06ffec8036d
Andrej Stevkovski <stevandrej@yahoo.com>
2020-02-06 02:06:32 +0100

    login
    check for cookie and login - done
    also changes width from 400px to 450px of the popup.html

--- popup.js
+++ popup.js
@@ -1,76 +1,115 @@
-login();
+(async function init() {
+	debugger;
+	let username = null
+	let password = null
+	let webaddress = null;
 
-function login(){
-    chrome.storage.local.get(["username", "password", "webaddress"], function (result) {
-      let { username, password, webaddress } = result;
+	if (await getCredentials()) {
+		await checkCookie();
+		await DownloadingTasks();
+	}
 
-      if (username == null || password == null || webaddress == null) {
-        document.getElementById('head').innerText = "Check credentials";
-        document.getElementById('logOut').hidden = true;
-        return 0;
-      }
-      else {
-        fetch(`${webaddress}/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=${username}&passwd=${password}&session=DownloadStation&format=cookie`)
-          .then(response => {
-            return response.json()
-          })
-          .then(loginrequest => {
-            chrome.cookies.set({
-              'url': `${webaddress}`,
-              'name': 'SynologyDownloadStation',
-              'value': loginrequest.data.sid
-            });
-          });
-      }
-    });
-    getDownloadTasks();
-}
+	async function getCredentials() {
+		return await new Promise(resolve => {
+			chrome.storage.local.get(["username", "password", "webaddress"], function (result) {
+				username = result.username;
+				password = result.password;
+				webaddress = result.webaddress;
+				if (username == null || password == null || webaddress == null) {
+					//If empty NOTIFY user and don't continue
+					document.getElementById('head').innerText = "Check credentials!";
+					document.getElementById('logOut').hidden = true;
+					document.getElementById('tasks-list-div').hidden = true;
+					document.getElementById('add-task').hidden = true;
+					resolve(false);
+				} else {
+					resolve(true);
+				}
+			})
+		});
+	}
 
-function getDownloadTasks() {
-  let tasks;
-  chrome.storage.local.get(["webaddress"], function (result) {
-    let webaddress = result.webaddress;
-    fetch(`${webaddress}/webapi/DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=list`).then(response => {
-      return response.json()
-    })
-      .then(data => {
-        tasks = data
-      })
-      .then(() => {
-        let appendActiveTasks = document.getElementById("tasks-list");
-        tasks.data.tasks.forEach(task => {
-          let li = document.createElement("li");
-          li.className = "list-group-item list-group-item-action";
-          li.appendChild(document.createTextNode(task.title));
-          appendActiveTasks.appendChild(li);
-        });
-      });
-  });
-}
+	async function checkCookie() {
+		chrome.cookies.get(
+			{
+				"url": webaddress,
+				"name": 'SynologyDownloadStation Chrome Extension'
+			}, async data => {
+			if (data) {
+				if (data.value == null && data.name !== "SynologyDownloadStation Chrome Extension") {
+					await login();
+				}
+			}
+			else {
+				await login();
+			}
+		});
+	}
 
-function addTask() {
-  chrome.storage.local.get(['webaddress'], function (result) {
-    let webaddress = result.webaddress;
-    let addTaskLink = document.getElementById('addTask').value;
-    if (addTaskLink != '')
-      fetch(`${webaddress}/webapi/DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=create&uri=${addTaskLink}`)
-        .then(console.log('Task ADDED!'));
-    else {
-      console.log("empty input field");
-    }
-  });
-}
+	async function login() {
+		await fetch(`${webaddress}/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=${username}&passwd=${password}&session=DownloadStation&format=cookie`)
+			.then(response => {
+				return response.json();
+			})
+			.then(loginrequest => {
+				chrome.cookies.set({
+					'url': `${webaddress}`,
+					'name': 'SynologyDownloadStation Chrome Extension',
+					'value': loginrequest.data.sid
+				});
+			});
+	}
 
-//--------------------
-//Button Listeners:
-//Button Add Task
-document.getElementById('buttonAddTask').addEventListener('click', function () {
-  addTask();
-});
+	async function DownloadingTasks() {
+		try {
+			await DownloadTasks_getMethod();
+		}
+		catch (er) {
+			await login();
+			await DownloadTasks_getMethod();
+		}
+	}
 
-//Log Out button
-document.getElementById('logOut').addEventListener('click', function () {
-  chrome.storage.local.get(["webaddress"], function (result) {
-    fetch(`${result.webaddress}/webapi/auth.cgi?api=SYNO.API.Auth&version=1&method=logout&session=DownloadStation`).then(chrome.storage.local.clear()).then(console.log('clear'));
-  });
-});
+	async function DownloadTasks_getMethod() {
+		await fetch(`${webaddress}/webapi/DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=list`)
+			.then(response => {
+				return response.json()
+			})
+			.then(tasks => {
+				console.log(tasks);
+				let appendActiveTasks = document.getElementById("tasks-list");
+				tasks.data.tasks.forEach(task => {
+					let li = document.createElement("li");
+					li.className = "list-group-item list-group-item-action";
+					li.appendChild(document.createTextNode(task.title));
+					appendActiveTasks.appendChild(li);
+				});
+			});
+	}
+
+	function addTask() {
+		chrome.storage.local.get(['webaddress'], function (result) {
+			let webaddress = result.webaddress;
+			const addTaskLink = document.getElementById('addTask').value;
+			if (addTaskLink !== '')
+				fetch(`${webaddress}/webapi/DownloadStation/task.cgi?api=SYNO.DownloadStation.Task&version=1&method=create&uri=${addTaskLink}`);
+		});
+	}
+
+	//--------------------
+	//Button Listeners:
+	//Button Add Task
+	document.getElementById('buttonAddTask').addEventListener('click', function () {
+		addTask();
+	});
+
+	//Log Out button
+	document.getElementById('logOut').addEventListener('click', function () {
+		console.log('log out');
+		chrome.storage.local.get(["webaddress"], function (result) {
+			fetch(`${result.webaddress}/webapi/auth.cgi?api=SYNO.API.Auth&version=1&method=logout&session=DownloadStation`).then(chrome.storage.local.clear()).then(console.log('clear'));
+		});
+		location.reload();
+	});
+
+})();
